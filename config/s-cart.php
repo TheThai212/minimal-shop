bb341e1c96d576f6e6d606adbdc4503c84c35ee0
The Thai MBP <thethai.nguyen.212@gmail.com>
2021-05-04 22:46:08 +0700

    push-master

---
+++ config/s-cart.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'version'     => '6.2',
+    'sub-version' => '6.2.0',
+    'type'        => 'basic',
+    'homepage'    => 'https://s-cart.org',
+    'title'       => 'Free Open Source eCommerce for Business',
+];
---
+++ database/migrations/2021_05_04_223239_add_quantity_to_shop_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddQuantityToShopProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(SC_DB_PREFIX.'shop_product', function (Blueprint $table) {
+            $table->integer('quantity')->nullable()->default(0);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(SC_DB_PREFIX.'shop_product', function (Blueprint $table) {
+            $table->dropColumn('quantity');
+        });
+    }
+}
